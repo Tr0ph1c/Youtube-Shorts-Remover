de9bdfbbfe2866824612220b6f30ed80b431031b
Tr0ph1c <ahmmahmouda767@gmail.com>
2022-10-28 20:42:54 +0200

    Its working now

--- background.js
+++ background.js
@@ -1,1 +1,9 @@
-
+chrome.tabs.onUpdated.addListener(function
+    (tabId, changeInfo, tab) {
+        if (changeInfo.url) {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+                chrome.tabs.sendMessage(tabId, {message: "NEW"});
+              });
+        }
+    }
+  );
--- content.js
+++ content.js
@@ -1,1 +1,7 @@
-
+chrome.runtime.onMessage.addListener(
+    function(request) {
+      if (request.message === "NEW") {
+        if (location.href.includes("shorts")) location.href += "&t=0";
+      }
+    }
+  );

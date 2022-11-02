846018109115e71a09e47e5efaafe5d86a718fd8
Tr0ph1c <ahmmahmouda767@gmail.com>
2022-11-02 18:06:21 +0200

    Bug Fix
    Fixed a bug that had to do with opening the shorts section of a channel.

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 chrome.runtime.onMessage.addListener(
     function(request) {
       if (request.message === "NEW") {
-        if (location.href.includes("shorts")) location.href += "&t=0";
+        if (location.href.includes("youtube.com/shorts")) location.href += "&t=0";
       }
     }
   );
